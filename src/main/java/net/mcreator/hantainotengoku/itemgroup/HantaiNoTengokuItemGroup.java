aff4abb664260008fd5a6e547e51982fe2744428
tetGhoul <kenzi@DESKTOP-2UDEV4P>
2020-11-05 11:56:25 -0500

    Added Creative Tab

---
+++ src/main/java/net/mcreator/hantainotengoku/itemgroup/HantaiNoTengokuItemGroup.java
@@ -0,0 +1,35 @@
+
+package net.mcreator.hantainotengoku.itemgroup;
+
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+
+import net.mcreator.hantainotengoku.item.TengokuIngotItem;
+import net.mcreator.hantainotengoku.HantaiNoTengokuModElements;
+
+@HantaiNoTengokuModElements.ModElement.Tag
+public class HantaiNoTengokuItemGroup extends HantaiNoTengokuModElements.ModElement {
+	public HantaiNoTengokuItemGroup(HantaiNoTengokuModElements instance) {
+		super(instance, 2);
+	}
+
+	@Override
+	public void initElements() {
+		tab = new ItemGroup("tabhantai_no_tengoku") {
+			@OnlyIn(Dist.CLIENT)
+			@Override
+			public ItemStack createIcon() {
+				return new ItemStack(TengokuIngotItem.block, (int) (1));
+			}
+
+			@OnlyIn(Dist.CLIENT)
+			public boolean hasSearchBar() {
+				return false;
+			}
+		};
+	}
+	public static ItemGroup tab;
+}
