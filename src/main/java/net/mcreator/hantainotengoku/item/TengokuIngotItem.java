0fb70fd271b6a08598e1e902903e9100f1593698
tetGhoul <kenzi@DESKTOP-2UDEV4P>
2020-11-05 11:55:24 -0500

    added tengoku ingot

---
+++ src/main/java/net/mcreator/hantainotengoku/item/TengokuIngotItem.java
@@ -0,0 +1,76 @@
+
+package net.mcreator.hantainotengoku.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.World;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.hantainotengoku.HantaiNoTengokuModElements;
+
+import java.util.List;
+
+@HantaiNoTengokuModElements.ModElement.Tag
+public class TengokuIngotItem extends HantaiNoTengokuModElements.ModElement {
+	@ObjectHolder("hantai_no_tengoku:tengoku_ingot")
+	public static final Item block = null;
+	public TengokuIngotItem(HantaiNoTengokuModElements instance) {
+		super(instance, 1);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ItemGroup.MISC).maxStackSize(1));
+			setRegistryName("tengoku_ingot");
+		}
+
+		@Override
+		public boolean hasContainerItem() {
+			return true;
+		}
+
+		@Override
+		public ItemStack getContainerItem(ItemStack itemstack) {
+			return new ItemStack(this);
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 5;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+
+		@Override
+		@OnlyIn(Dist.CLIENT)
+		public boolean hasEffect(ItemStack itemstack) {
+			return true;
+		}
+
+		@Override
+		public void addInformation(ItemStack itemstack, World world, List<ITextComponent> list, ITooltipFlag flag) {
+			super.addInformation(itemstack, world, list, flag);
+			list.add(new StringTextComponent("core from heaven"));
+		}
+	}
+}
