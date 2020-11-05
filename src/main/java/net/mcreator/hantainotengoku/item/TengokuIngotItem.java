8cdfdcb9856ed958bef9522dc3cc7d4d84d4b1f8
tetGhoul <kenzi@DESKTOP-2UDEV4P>
2020-11-05 12:50:17 -0500

    Tengoku Ingots now take damage when being used in crafting grid

--- src/main/java/net/mcreator/hantainotengoku/item/TengokuIngotItem.java
+++ src/main/java/net/mcreator/hantainotengoku/item/TengokuIngotItem.java
@@ -10,6 +10,9 @@ import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Item;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.block.BlockState;
 
@@ -17,6 +20,8 @@ import net.mcreator.hantainotengoku.itemgroup.HantaiNoTengokuItemGroup;
 import net.mcreator.hantainotengoku.HantaiNoTengokuModElements;
 
 import java.util.List;
+
+import com.google.common.collect.Multimap;
 
 @HantaiNoTengokuModElements.ModElement.Tag
 public class TengokuIngotItem extends HantaiNoTengokuModElements.ModElement {
@@ -59,6 +64,18 @@ public class TengokuIngotItem extends HantaiNoTengokuModElements.ModElement {
 		@Override
 		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
 			return 1F;
+		}
+
+		@Override
+		public Multimap<String, AttributeModifier> getAttributeModifiers(EquipmentSlotType slot) {
+			Multimap<String, AttributeModifier> multimap = super.getAttributeModifiers(slot);
+			if (slot == EquipmentSlotType.MAINHAND) {
+				multimap.put(SharedMonsterAttributes.ATTACK_DAMAGE.getName(),
+						new AttributeModifier(ATTACK_DAMAGE_MODIFIER, "item_damage", (double) -2, AttributeModifier.Operation.ADDITION));
+				multimap.put(SharedMonsterAttributes.ATTACK_SPEED.getName(),
+						new AttributeModifier(ATTACK_SPEED_MODIFIER, "item_attack_speed", -2.4, AttributeModifier.Operation.ADDITION));
+			}
+			return multimap;
 		}
 
 		@Override
