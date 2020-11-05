7d28300a220f0645b5caf0aa1c549a1879f43b9d
tetGhoul <kenzi@DESKTOP-2UDEV4P>
2020-11-05 12:36:14 -0500

    Added Tengoku Infused Diamond

---
+++ src/main/java/net/mcreator/hantainotengoku/item/TengokuInfusedDiamondItem.java
@@ -0,0 +1,66 @@
+
+package net.mcreator.hantainotengoku.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.World;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.hantainotengoku.itemgroup.HantaiNoTengokuItemGroup;
+import net.mcreator.hantainotengoku.HantaiNoTengokuModElements;
+
+import java.util.List;
+
+@HantaiNoTengokuModElements.ModElement.Tag
+public class TengokuInfusedDiamondItem extends HantaiNoTengokuModElements.ModElement {
+	@ObjectHolder("hantai_no_tengoku:tengoku_infused_diamond")
+	public static final Item block = null;
+	public TengokuInfusedDiamondItem(HantaiNoTengokuModElements instance) {
+		super(instance, 4);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(HantaiNoTengokuItemGroup.tab).maxStackSize(16));
+			setRegistryName("tengoku_infused_diamond");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+
+		@Override
+		@OnlyIn(Dist.CLIENT)
+		public boolean hasEffect(ItemStack itemstack) {
+			return true;
+		}
+
+		@Override
+		public void addInformation(ItemStack itemstack, World world, List<ITextComponent> list, ITooltipFlag flag) {
+			super.addInformation(itemstack, world, list, flag);
+			list.add(new StringTextComponent("Heaven INfused Diamond"));
+		}
+	}
+}
