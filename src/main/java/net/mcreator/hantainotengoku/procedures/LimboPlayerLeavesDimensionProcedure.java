9e01914306cde157a65c0fb5912ab1040aa31b1f
tetGhoul <kenzi@DESKTOP-2UDEV4P>
2020-11-05 20:46:40 -0500

    Tengoku Obsidian

---
+++ src/main/java/net/mcreator/hantainotengoku/procedures/LimboPlayerLeavesDimensionProcedure.java
@@ -0,0 +1,55 @@
+package net.mcreator.hantainotengoku.procedures;
+
+import net.minecraft.world.server.ServerWorld;
+import net.minecraft.world.IWorld;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.math.Vec2f;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.command.ICommandSource;
+import net.minecraft.command.FunctionObject;
+import net.minecraft.command.CommandSource;
+
+import net.mcreator.hantainotengoku.HantaiNoTengokuModElements;
+
+import java.util.Optional;
+import java.util.Map;
+
+@HantaiNoTengokuModElements.ModElement.Tag
+public class LimboPlayerLeavesDimensionProcedure extends HantaiNoTengokuModElements.ModElement {
+	public LimboPlayerLeavesDimensionProcedure(HantaiNoTengokuModElements instance) {
+		super(instance, 10);
+	}
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("x") == null) {
+			System.err.println("Failed to load dependency x for procedure LimboPlayerLeavesDimension!");
+			return;
+		}
+		if (dependencies.get("y") == null) {
+			System.err.println("Failed to load dependency y for procedure LimboPlayerLeavesDimension!");
+			return;
+		}
+		if (dependencies.get("z") == null) {
+			System.err.println("Failed to load dependency z for procedure LimboPlayerLeavesDimension!");
+			return;
+		}
+		if (dependencies.get("world") == null) {
+			System.err.println("Failed to load dependency world for procedure LimboPlayerLeavesDimension!");
+			return;
+		}
+		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
+		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
+		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
+		IWorld world = (IWorld) dependencies.get("world");
+		if (!world.getWorld().isRemote && world.getWorld().getServer() != null) {
+			Optional<FunctionObject> _fopt = world.getWorld().getServer().getFunctionManager()
+					.get(new ResourceLocation("The whispers start to quiet down and fade away, you dont want to go back"));
+			if (_fopt.isPresent()) {
+				FunctionObject _fobj = _fopt.get();
+				world.getWorld().getServer().getFunctionManager().execute(_fobj, new CommandSource(ICommandSource.DUMMY, new Vec3d(x, y, z),
+						Vec2f.ZERO, (ServerWorld) world.getWorld(), 4, "", new StringTextComponent(""), world.getWorld().getServer(), null));
+			}
+		}
+	}
+}
