ff442a31439b41db25ea7c577d65657e6b098ef3
tetGhoul <kenzi@DESKTOP-2UDEV4P>
2020-11-08 00:21:28 -0500

    Added Demon Essence stick shit

---
+++ src/main/java/net/mcreator/hantainotengoku/item/DemonEssenceInfusedStickItem.java
@@ -0,0 +1,63 @@
+
+package net.mcreator.hantainotengoku.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.hantainotengoku.itemgroup.HantaiNoTengokuItemGroup;
+import net.mcreator.hantainotengoku.HantaiNoTengokuModElements;
+
+import com.google.common.collect.Multimap;
+
+@HantaiNoTengokuModElements.ModElement.Tag
+public class DemonEssenceInfusedStickItem extends HantaiNoTengokuModElements.ModElement {
+	@ObjectHolder("hantai_no_tengoku:demon_essence_infused_stick")
+	public static final Item block = null;
+	public DemonEssenceInfusedStickItem(HantaiNoTengokuModElements instance) {
+		super(instance, 35);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(HantaiNoTengokuItemGroup.tab).maxStackSize(64));
+			setRegistryName("demon_essence_infused_stick");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+
+		@Override
+		public Multimap<String, AttributeModifier> getAttributeModifiers(EquipmentSlotType slot) {
+			Multimap<String, AttributeModifier> multimap = super.getAttributeModifiers(slot);
+			if (slot == EquipmentSlotType.MAINHAND) {
+				multimap.put(SharedMonsterAttributes.ATTACK_DAMAGE.getName(),
+						new AttributeModifier(ATTACK_DAMAGE_MODIFIER, "item_damage", (double) -2, AttributeModifier.Operation.ADDITION));
+				multimap.put(SharedMonsterAttributes.ATTACK_SPEED.getName(),
+						new AttributeModifier(ATTACK_SPEED_MODIFIER, "item_attack_speed", -2.4, AttributeModifier.Operation.ADDITION));
+			}
+			return multimap;
+		}
+	}
+}
