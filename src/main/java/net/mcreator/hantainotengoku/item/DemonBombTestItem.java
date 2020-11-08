d4d1144596a82a0fb65fed19de3711d1ca33a6b0
tetGhoul <kenzi@DESKTOP-2UDEV4P>
2020-11-07 20:48:30 -0500

    poopity scoop

--- src/main/java/net/mcreator/hantainotengoku/item/DemonBombTestItem.java
+++ src/main/java/net/mcreator/hantainotengoku/item/DemonBombTestItem.java
@@ -1,15 +1,52 @@
 
 package net.mcreator.hantainotengoku.item;
 
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.fml.network.NetworkHooks;
+import net.minecraftforge.fml.network.FMLPlayMessages;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.client.registry.RenderingRegistry;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.World;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.Hand;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.ActionResult;
+import net.minecraft.network.IPacket;
+import net.minecraft.item.UseAction;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.entity.projectile.AbstractArrowEntity;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.IRendersAsItem;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.EntityClassification;
+import net.minecraft.entity.Entity;
+import net.minecraft.client.renderer.entity.SpriteRenderer;
+import net.minecraft.client.Minecraft;
+
+import net.mcreator.hantainotengoku.procedures.DemonBombTestBulletHitsBlockProcedure;
+import net.mcreator.hantainotengoku.procedures.DemonBombBulletHitsPlayerProcedure;
+import net.mcreator.hantainotengoku.itemgroup.HantaiNoTengokuItemGroup;
+import net.mcreator.hantainotengoku.HantaiNoTengokuModElements;
+
+import java.util.Random;
+import java.util.Map;
+import java.util.HashMap;
+
 @HantaiNoTengokuModElements.ModElement.Tag
 public class DemonBombTestItem extends HantaiNoTengokuModElements.ModElement {
-
 	@ObjectHolder("hantai_no_tengoku:demon_bomb_test")
 	public static final Item block = null;
-
 	@ObjectHolder("hantai_no_tengoku:entitybulletdemon_bomb_test")
 	public static final EntityType arrow = null;
-
 	public DemonBombTestItem(HantaiNoTengokuModElements instance) {
 		super(instance, 26);
 	}
@@ -28,12 +65,9 @@ public class DemonBombTestItem extends HantaiNoTengokuModElements.ModElement {
 		RenderingRegistry.registerEntityRenderingHandler(arrow,
 				renderManager -> new SpriteRenderer(renderManager, Minecraft.getInstance().getItemRenderer()));
 	}
-
 	public static class ItemRanged extends Item {
-
 		public ItemRanged() {
 			super(new Item.Properties().group(HantaiNoTengokuItemGroup.tab).maxDamage(100));
-
 			setRegistryName("demon_bomb_test");
 		}
 
@@ -61,22 +95,16 @@ public class DemonBombTestItem extends HantaiNoTengokuModElements.ModElement {
 				double y = entity.getPosY();
 				double z = entity.getPosZ();
 				if (true) {
-
 					ArrowCustomEntity entityarrow = shoot(world, entity, random, 1f, 3.9999999999999996, 2);
-
 					itemstack.damageItem(1, entity, e -> e.sendBreakAnimation(entity.getActiveHand()));
-
 					entityarrow.pickupStatus = AbstractArrowEntity.PickupStatus.DISALLOWED;
-
 				}
 			}
 		}
-
 	}
 
 	@OnlyIn(value = Dist.CLIENT, _interface = IRendersAsItem.class)
 	public static class ArrowCustomEntity extends AbstractArrowEntity implements IRendersAsItem {
-
 		public ArrowCustomEntity(FMLPlayMessages.SpawnEntity packet, World world) {
 			super(arrow, world);
 		}
@@ -119,12 +147,10 @@ public class DemonBombTestItem extends HantaiNoTengokuModElements.ModElement {
 			World world = this.world;
 			{
 				Map<String, Object> $_dependencies = new HashMap<>();
-
 				$_dependencies.put("x", x);
 				$_dependencies.put("y", y);
 				$_dependencies.put("z", z);
 				$_dependencies.put("world", world);
-
 				DemonBombTestBulletHitsBlockProcedure.executeProcedure($_dependencies);
 			}
 		}
@@ -140,12 +166,10 @@ public class DemonBombTestItem extends HantaiNoTengokuModElements.ModElement {
 			World world = this.world;
 			{
 				Map<String, Object> $_dependencies = new HashMap<>();
-
 				$_dependencies.put("x", x);
 				$_dependencies.put("y", y);
 				$_dependencies.put("z", z);
 				$_dependencies.put("world", world);
-
 				DemonBombTestBulletHitsBlockProcedure.executeProcedure($_dependencies);
 			}
 		}
@@ -160,28 +184,22 @@ public class DemonBombTestItem extends HantaiNoTengokuModElements.ModElement {
 			Entity entity = this.getShooter();
 			{
 				Map<String, Object> $_dependencies = new HashMap<>();
-
 				$_dependencies.put("entity", entity);
-
 				DemonBombBulletHitsPlayerProcedure.executeProcedure($_dependencies);
 			}
 			if (this.inGround) {
 				{
 					Map<String, Object> $_dependencies = new HashMap<>();
-
 					$_dependencies.put("x", x);
 					$_dependencies.put("y", y);
 					$_dependencies.put("z", z);
 					$_dependencies.put("world", world);
-
 					DemonBombTestBulletHitsBlockProcedure.executeProcedure($_dependencies);
 				}
 				this.remove();
 			}
 		}
-
 	}
-
 	public static ArrowCustomEntity shoot(World world, LivingEntity entity, Random random, float power, double damage, int knockback) {
 		ArrowCustomEntity entityarrow = new ArrowCustomEntity(arrow, entity, world);
 		entityarrow.shoot(entity.getLookVec().x, entity.getLookVec().y, entity.getLookVec().z, power * 2, 0);
@@ -190,14 +208,12 @@ public class DemonBombTestItem extends HantaiNoTengokuModElements.ModElement {
 		entityarrow.setDamage(damage);
 		entityarrow.setKnockbackStrength(knockback);
 		world.addEntity(entityarrow);
-
 		double x = entity.getPosX();
 		double y = entity.getPosY();
 		double z = entity.getPosZ();
 		world.playSound((PlayerEntity) null, (double) x, (double) y, (double) z,
 				(net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("entity.arrow.shoot")),
 				SoundCategory.PLAYERS, 1, 1f / (random.nextFloat() * 0.5f + 1) + (power / 2));
-
 		return entityarrow;
 	}
 
@@ -207,21 +223,17 @@ public class DemonBombTestItem extends HantaiNoTengokuModElements.ModElement {
 		double d1 = target.getPosX() - entity.getPosX();
 		double d3 = target.getPosZ() - entity.getPosZ();
 		entityarrow.shoot(d1, d0 - entityarrow.getPosY() + (double) MathHelper.sqrt(d1 * d1 + d3 * d3) * 0.2F, d3, 1f * 2, 12.0F);
-
 		entityarrow.setSilent(true);
 		entityarrow.setDamage(3.9999999999999996);
 		entityarrow.setKnockbackStrength(2);
 		entityarrow.setIsCritical(false);
 		entity.world.addEntity(entityarrow);
-
 		double x = entity.getPosX();
 		double y = entity.getPosY();
 		double z = entity.getPosZ();
 		entity.world.playSound((PlayerEntity) null, (double) x, (double) y, (double) z,
 				(net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("entity.arrow.shoot")),
 				SoundCategory.PLAYERS, 1, 1f / (new Random().nextFloat() * 0.5f + 1));
-
 		return entityarrow;
 	}
-
 }
--- src/main/java/net/mcreator/hantainotengoku/procedures/DemonBombTestBulletHitsLivingEntityProcedure.java
+++ src/main/java/net/mcreator/hantainotengoku/procedures/DemonBombTestBulletHitsLivingEntityProcedure.java
@@ -1,11 +1,15 @@
 package net.mcreator.hantainotengoku.procedures;
 
+import net.minecraft.entity.Entity;
+
+import net.mcreator.hantainotengoku.HantaiNoTengokuModElements;
+
+import java.util.Map;
+
 @HantaiNoTengokuModElements.ModElement.Tag
 public class DemonBombTestBulletHitsLivingEntityProcedure extends HantaiNoTengokuModElements.ModElement {
-
 	public DemonBombTestBulletHitsLivingEntityProcedure(HantaiNoTengokuModElements instance) {
 		super(instance, 26);
-
 	}
 
 	public static void executeProcedure(Map<String, Object> dependencies) {
@@ -13,11 +17,7 @@ public class DemonBombTestBulletHitsLivingEntityProcedure extends HantaiNoTengok
 			System.err.println("Failed to load dependency entity for procedure DemonBombTestBulletHitsLivingEntity!");
 			return;
 		}
-
 		Entity entity = (Entity) dependencies.get("entity");
-
 		entity.setFire((int) 15);
-
 	}
-
 }
