f71b99207ae9fe4217311ee48055f5a5346ce9df
tetGhoul <kenzi@DESKTOP-2UDEV4P>
2020-11-07 23:40:39 -0500

    DemonEssenceLootTable added

---
+++ src/main/java/net/mcreator/hantainotengoku/item/DemonEssenceItem.java
@@ -0,0 +1,54 @@
+
+package net.mcreator.hantainotengoku.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.hantainotengoku.itemgroup.HantaiNoTengokuItemGroup;
+import net.mcreator.hantainotengoku.HantaiNoTengokuModElements;
+
+@HantaiNoTengokuModElements.ModElement.Tag
+public class DemonEssenceItem extends HantaiNoTengokuModElements.ModElement {
+	@ObjectHolder("hantai_no_tengoku:demon_essence")
+	public static final Item block = null;
+	public DemonEssenceItem(HantaiNoTengokuModElements instance) {
+		super(instance, 33);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(HantaiNoTengokuItemGroup.tab).maxStackSize(64));
+			setRegistryName("demon_essence");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+
+		@Override
+		@OnlyIn(Dist.CLIENT)
+		public boolean hasEffect(ItemStack itemstack) {
+			return true;
+		}
+	}
+}
