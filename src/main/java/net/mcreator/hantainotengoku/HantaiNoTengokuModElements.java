7f5f41b70fa4579219dee45bc536eae0de821854
KonoRequiem <jahsi@DESKTOP-ADV207E>
2020-11-09 18:47:59 -0500

    Gonna Fix Grolem

--- src/main/java/net/mcreator/hantainotengoku/HantaiNoTengokuModElements.java
+++ src/main/java/net/mcreator/hantainotengoku/HantaiNoTengokuModElements.java
@@ -13,6 +13,7 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.api.distmarker.Dist;
 
@@ -66,6 +67,7 @@ public class HantaiNoTengokuModElements {
 		}
 		Collections.sort(elements);
 		elements.forEach(HantaiNoTengokuModElements.ModElement::initElements);
+		MinecraftForge.EVENT_BUS.register(new HantaiNoTengokuModVariables(this));
 	}
 
 	public void registerSounds(RegistryEvent.Register<net.minecraft.util.SoundEvent> event) {
