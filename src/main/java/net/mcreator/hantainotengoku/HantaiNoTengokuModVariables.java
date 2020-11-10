ba8b7731926ae8742bf813c3082a9f4115264b67
kenzi <kenzi@DESKTOP-2UDEV4P>
2020-11-10 16:06:49 -0500

    MCreator Merge commit 'e1c695cfb75d99facfb09e040158c4138109039b' into 1c65e85ffc6267114c2313db4ac94a8ec55e2e03

--- src/main/java/net/mcreator/hantainotengoku/HantaiNoTengokuModVariables.java
+++ src/main/java/net/mcreator/hantainotengoku/HantaiNoTengokuModVariables.java
@@ -2,153 +2,142 @@ package net.mcreator.hantainotengoku;
 
 import net.minecraftforge.fml.network.PacketDistributor;
 import net.minecraftforge.fml.network.NetworkEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.event.AttachCapabilitiesEvent;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.common.util.FakePlayer;
+import net.minecraftforge.common.capabilities.ICapabilitySerializable;
+import net.minecraftforge.common.capabilities.CapabilityManager;
+import net.minecraftforge.common.capabilities.CapabilityInject;
+import net.minecraftforge.common.capabilities.Capability;
 
-import net.minecraft.world.storage.WorldSavedData;
-import net.minecraft.world.server.ServerWorld;
-import net.minecraft.world.dimension.DimensionType;
-import net.minecraft.world.IWorld;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.Direction;
 import net.minecraft.network.PacketBuffer;
+import net.minecraft.nbt.INBT;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.client.Minecraft;
 
 import java.util.function.Supplier;
 
 public class HantaiNoTengokuModVariables {
 	public HantaiNoTengokuModVariables(HantaiNoTengokuModElements elements) {
-		elements.addNetworkMessage(WorldSavedDataSyncMessage.class, WorldSavedDataSyncMessage::buffer, WorldSavedDataSyncMessage::new,
-				WorldSavedDataSyncMessage::handler);
+		elements.addNetworkMessage(PlayerVariablesSyncMessage.class, PlayerVariablesSyncMessage::buffer, PlayerVariablesSyncMessage::new,
+				PlayerVariablesSyncMessage::handler);
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::init);
 	}
 
+	private void init(FMLCommonSetupEvent event) {
+		CapabilityManager.INSTANCE.register(PlayerVariables.class, new PlayerVariablesStorage(), PlayerVariables::new);
+	}
+	@CapabilityInject(PlayerVariables.class)
+	public static Capability<PlayerVariables> PLAYER_VARIABLES_CAPABILITY = null;
 	@SubscribeEvent
-	public void onPlayerLoggedIn(PlayerEvent.PlayerLoggedInEvent event) {
-		if (!event.getPlayer().world.isRemote) {
-			WorldSavedData mapdata = MapVariables.get(event.getPlayer().world);
-			WorldSavedData worlddata = WorldVariables.get(event.getPlayer().world);
-			if (mapdata != null)
-				HantaiNoTengokuMod.PACKET_HANDLER.send(PacketDistributor.PLAYER.with(() -> (ServerPlayerEntity) event.getPlayer()),
-						new WorldSavedDataSyncMessage(0, mapdata));
-			if (worlddata != null)
-				HantaiNoTengokuMod.PACKET_HANDLER.send(PacketDistributor.PLAYER.with(() -> (ServerPlayerEntity) event.getPlayer()),
-						new WorldSavedDataSyncMessage(1, worlddata));
-		}
+	public void onAttachCapabilities(AttachCapabilitiesEvent<Entity> event) {
+		if (event.getObject() instanceof PlayerEntity && !(event.getObject() instanceof FakePlayer))
+			event.addCapability(new ResourceLocation("hantai_no_tengoku", "player_variables"), new PlayerVariablesProvider());
 	}
-
-	@SubscribeEvent
-	public void onPlayerChangedDimension(PlayerEvent.PlayerChangedDimensionEvent event) {
-		if (!event.getPlayer().world.isRemote) {
-			WorldSavedData worlddata = WorldVariables.get(event.getPlayer().world);
-			if (worlddata != null)
-				HantaiNoTengokuMod.PACKET_HANDLER.send(PacketDistributor.PLAYER.with(() -> (ServerPlayerEntity) event.getPlayer()),
-						new WorldSavedDataSyncMessage(1, worlddata));
-		}
-	}
-	public static class WorldVariables extends WorldSavedData {
-		public static final String DATA_NAME = "hantai_no_tengoku_worldvars";
-		public WorldVariables() {
-			super(DATA_NAME);
-		}
-
-		public WorldVariables(String s) {
-			super(s);
+	private static class PlayerVariablesProvider implements ICapabilitySerializable<INBT> {
+		private final LazyOptional<PlayerVariables> instance = LazyOptional.of(PLAYER_VARIABLES_CAPABILITY::getDefaultInstance);
+		@Override
+		public <T> LazyOptional<T> getCapability(Capability<T> cap, Direction side) {
+			return cap == PLAYER_VARIABLES_CAPABILITY ? instance.cast() : LazyOptional.empty();
 		}
 
 		@Override
-		public void read(CompoundNBT nbt) {
+		public INBT serializeNBT() {
+			return PLAYER_VARIABLES_CAPABILITY.getStorage().writeNBT(PLAYER_VARIABLES_CAPABILITY, this.instance.orElseThrow(RuntimeException::new),
+					null);
 		}
 
 		@Override
-		public CompoundNBT write(CompoundNBT nbt) {
+		public void deserializeNBT(INBT nbt) {
+			PLAYER_VARIABLES_CAPABILITY.getStorage().readNBT(PLAYER_VARIABLES_CAPABILITY, this.instance.orElseThrow(RuntimeException::new), null,
+					nbt);
+		}
+	}
+
+	private static class PlayerVariablesStorage implements Capability.IStorage<PlayerVariables> {
+		@Override
+		public INBT writeNBT(Capability<PlayerVariables> capability, PlayerVariables instance, Direction side) {
+			CompoundNBT nbt = new CompoundNBT();
+			nbt.putDouble("mana", instance.mana);
 			return nbt;
 		}
 
-		public void syncData(IWorld world) {
-			this.markDirty();
-			if (!world.getWorld().isRemote)
-				HantaiNoTengokuMod.PACKET_HANDLER.send(PacketDistributor.DIMENSION.with(world.getWorld().dimension::getType),
-						new WorldSavedDataSyncMessage(1, this));
-		}
-		static WorldVariables clientSide = new WorldVariables();
-		public static WorldVariables get(IWorld world) {
-			if (world.getWorld() instanceof ServerWorld) {
-				return ((ServerWorld) world.getWorld()).getSavedData().getOrCreate(WorldVariables::new, DATA_NAME);
-			} else {
-				return clientSide;
-			}
+		@Override
+		public void readNBT(Capability<PlayerVariables> capability, PlayerVariables instance, Direction side, INBT inbt) {
+			CompoundNBT nbt = (CompoundNBT) inbt;
+			instance.mana = nbt.getDouble("mana");
 		}
 	}
 
-	public static class MapVariables extends WorldSavedData {
-		public static final String DATA_NAME = "hantai_no_tengoku_mapvars";
-		public boolean idle = false;
-		public boolean walking = false;
-		public boolean attacking = false;
-		public MapVariables() {
-			super(DATA_NAME);
-		}
-
-		public MapVariables(String s) {
-			super(s);
-		}
-
-		@Override
-		public void read(CompoundNBT nbt) {
-			idle = nbt.getBoolean("idle");
-			walking = nbt.getBoolean("walking");
-			attacking = nbt.getBoolean("attacking");
-		}
-
-		@Override
-		public CompoundNBT write(CompoundNBT nbt) {
-			nbt.putBoolean("idle", idle);
-			nbt.putBoolean("walking", walking);
-			nbt.putBoolean("attacking", attacking);
-			return nbt;
-		}
-
-		public void syncData(IWorld world) {
-			this.markDirty();
-			if (!world.getWorld().isRemote)
-				HantaiNoTengokuMod.PACKET_HANDLER.send(PacketDistributor.ALL.noArg(), new WorldSavedDataSyncMessage(0, this));
-		}
-		static MapVariables clientSide = new MapVariables();
-		public static MapVariables get(IWorld world) {
-			if (world.getWorld() instanceof ServerWorld) {
-				return world.getWorld().getServer().getWorld(DimensionType.OVERWORLD).getSavedData().getOrCreate(MapVariables::new, DATA_NAME);
-			} else {
-				return clientSide;
-			}
+	public static class PlayerVariables {
+		public double mana = 100.0;
+		public void syncPlayerVariables(Entity entity) {
+			if (entity instanceof ServerPlayerEntity)
+				HantaiNoTengokuMod.PACKET_HANDLER.send(PacketDistributor.PLAYER.with(() -> (ServerPlayerEntity) entity),
+						new PlayerVariablesSyncMessage(this));
 		}
 	}
+	@SubscribeEvent
+	public void onPlayerLoggedInSyncPlayerVariables(PlayerEvent.PlayerLoggedInEvent event) {
+		if (!event.getPlayer().world.isRemote)
+			((PlayerVariables) event.getPlayer().getCapability(PLAYER_VARIABLES_CAPABILITY, null).orElse(new PlayerVariables()))
+					.syncPlayerVariables(event.getPlayer());
+	}
 
-	public static class WorldSavedDataSyncMessage {
-		public int type;
-		public WorldSavedData data;
-		public WorldSavedDataSyncMessage(PacketBuffer buffer) {
-			this.type = buffer.readInt();
-			this.data = this.type == 0 ? new MapVariables() : new WorldVariables();
-			this.data.read(buffer.readCompoundTag());
+	@SubscribeEvent
+	public void onPlayerRespawnedSyncPlayerVariables(PlayerEvent.PlayerRespawnEvent event) {
+		if (!event.getPlayer().world.isRemote)
+			((PlayerVariables) event.getPlayer().getCapability(PLAYER_VARIABLES_CAPABILITY, null).orElse(new PlayerVariables()))
+					.syncPlayerVariables(event.getPlayer());
+	}
+
+	@SubscribeEvent
+	public void onPlayerChangedDimensionSyncPlayerVariables(PlayerEvent.PlayerChangedDimensionEvent event) {
+		if (!event.getPlayer().world.isRemote)
+			((PlayerVariables) event.getPlayer().getCapability(PLAYER_VARIABLES_CAPABILITY, null).orElse(new PlayerVariables()))
+					.syncPlayerVariables(event.getPlayer());
+	}
+
+	@SubscribeEvent
+	public void clonePlayer(PlayerEvent.Clone event) {
+		if (event.isWasDeath()) {
+			PlayerVariables original = ((PlayerVariables) event.getOriginal().getCapability(PLAYER_VARIABLES_CAPABILITY, null)
+					.orElse(new PlayerVariables()));
+			PlayerVariables clone = ((PlayerVariables) event.getEntity().getCapability(PLAYER_VARIABLES_CAPABILITY, null)
+					.orElse(new PlayerVariables()));
+		}
+	}
+	public static class PlayerVariablesSyncMessage {
+		public PlayerVariables data;
+		public PlayerVariablesSyncMessage(PacketBuffer buffer) {
+			this.data = new PlayerVariables();
+			new PlayerVariablesStorage().readNBT(null, this.data, null, buffer.readCompoundTag());
 		}
 
-		public WorldSavedDataSyncMessage(int type, WorldSavedData data) {
-			this.type = type;
+		public PlayerVariablesSyncMessage(PlayerVariables data) {
 			this.data = data;
 		}
 
-		public static void buffer(WorldSavedDataSyncMessage message, PacketBuffer buffer) {
-			buffer.writeInt(message.type);
-			buffer.writeCompoundTag(message.data.write(new CompoundNBT()));
+		public static void buffer(PlayerVariablesSyncMessage message, PacketBuffer buffer) {
+			buffer.writeCompoundTag((CompoundNBT) new PlayerVariablesStorage().writeNBT(null, message.data, null));
 		}
 
-		public static void handler(WorldSavedDataSyncMessage message, Supplier<NetworkEvent.Context> contextSupplier) {
+		public static void handler(PlayerVariablesSyncMessage message, Supplier<NetworkEvent.Context> contextSupplier) {
 			NetworkEvent.Context context = contextSupplier.get();
 			context.enqueueWork(() -> {
 				if (!context.getDirection().getReceptionSide().isServer()) {
-					if (message.type == 0)
-						MapVariables.clientSide = (MapVariables) message.data;
-					else
-						WorldVariables.clientSide = (WorldVariables) message.data;
+					PlayerVariables variables = ((PlayerVariables) Minecraft.getInstance().player.getCapability(PLAYER_VARIABLES_CAPABILITY, null)
+							.orElse(new PlayerVariables()));
+					variables.mana = message.data.mana;
 				}
 			});
 			context.setPacketHandled(true);
